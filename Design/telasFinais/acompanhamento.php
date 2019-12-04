466abb0cbd13e10477e9b318a02a7ec740a7266e
lucassoaresgranja <32617747+lucassoaresgranja@users.noreply.github.com>
2019-12-04 16:16:55 -0200

    Merge origin/master
    Conflicts:
    	nbproject/project.xml

--- Design/telasFinais/acompanhamento.php
+++ Design/telasFinais/acompanhamento.php
@@ -270,160 +270,160 @@
                 </div>
 
             </div>
-
+            
             <div class="card">
 
 
-                <div class="row">
-                    </br>
-                    <h5 class="center">ATIVIDADE</h5>
-                    <div class="col s12 m12 l12"
-                         <!-- PROPRIEDADE PARA CHAMAR AS INFORMAÇÕES DO BANCO DE DADOS 
-                         E APRESENTAR EM UM BOTAO NA TABELA, ESSE BOTAO IRAR USAR UM MODAL 
-                         PARA APRESENTAR AS INFORMAÇÕES NA TELA-->
+            <div class="row">
+                </br>
+                <h5 class="center">ATIVIDADE</h5>
+                <div class="col s12 m12 l12"
+                     <!-- PROPRIEDADE PARA CHAMAR AS INFORMAÇÕES DO BANCO DE DADOS 
+                     E APRESENTAR EM UM BOTAO NA TABELA, ESSE BOTAO IRAR USAR UM MODAL 
+                     PARA APRESENTAR AS INFORMAÇÕES NA TELA-->
 
-                         <div id="descicaoAtividade" class="modal">
-                            <div class="modal-content">
-                                <h4>Descrição</h4>
-                                <p>Informações</p>
-                            </div>
-                            <div class="modal-footer">
-                                <a href="#!" class="modal-close waves-effect waves-green btn-flat">Fechar</a>
-                            </div>
+                     <div id="descicaoAtividade" class="modal">
+                        <div class="modal-content">
+                            <h4>Descrição</h4>
+                            <p>Informações</p>
                         </div>
+                        <div class="modal-footer">
+                            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Fechar</a>
+                        </div>
+                    </div>
 
-                        <div id="descicaoAtividade" class="modal">
-                            <div class="modal-content">
-                                <h4>Descrição</h4>
-                                <p>Informações</p>
-                            </div>
-                            <div class="modal-footer">
-                                <a href="#!" class="modal-close waves-effect waves-green btn-flat">Fechar</a>
-                            </div>
+                    <div id="descicaoAtividade" class="modal">
+                        <div class="modal-content">
+                            <h4>Descrição</h4>
+                            <p>Informações</p>
                         </div>
+                        <div class="modal-footer">
+                            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Fechar</a>
+                        </div>
+                    </div>
 
-                        <div id="resultadosEsperadosAtividade" class="modal">
-                            <div class="modal-content">
-                                <h4>Resultados Esperados</h4>
-                                <p>Informações</p>
-                            </div>
-                            <div class="modal-footer">
-                                <a href="#!" class="modal-close waves-effect waves-green btn-flat">Fechar</a>
-                            </div>
+                    <div id="resultadosEsperadosAtividade" class="modal">
+                        <div class="modal-content">
+                            <h4>Resultados Esperados</h4>
+                            <p>Informações</p>
                         </div>
+                        <div class="modal-footer">
+                            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Fechar</a>
+                        </div>
+                    </div>
 
-                        <div id="resultadosObtidosAtividade" class="modal">
-                            <div class="modal-content">
-                                <h4>Resultados Obtidos</h4>
-                                <p>Informações</p>
-                            </div>
-                            <div class="modal-footer">
-                                <a href="#!" class="modal-close waves-effect waves-green btn-flat">Fechar</a>
-                            </div>
+                    <div id="resultadosObtidosAtividade" class="modal">
+                        <div class="modal-content">
+                            <h4>Resultados Obtidos</h4>
+                            <p>Informações</p>
                         </div>
+                        <div class="modal-footer">
+                            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Fechar</a>
+                        </div>
+                    </div>
 
                         <!--<input type="text" id="search" placeholder="Type to search..." />-->
-                        <table cellpadding="1" cellspacing="1" class="table table-hover responsive-table centered " id="tabelaFicha">
-                            <thead>
-                                <tr class="center-align">
-                                    <th>Título</th>
-                                    <th>Servidor</th>
-                                    <th>Data Inicio</th>
-                                    <th>Data Fim</th>
-                                    <th>Descrição</th>
-                                    <th>Res.. Esperados</th>
-                                    <th>Res.. Obtidos</th>
-                                    <th>Ações</th>
+                    <table cellpadding="1" cellspacing="1" class="table table-hover responsive-table centered " id="tabelaFicha">
+                        <thead>
+                            <tr class="center-align">
+                                <th>Título</th>
+                                <th>Servidor</th>
+                                <th>Data Inicio</th>
+                                <th>Data Fim</th>
+                                <th>Descrição</th>
+                                <th>Res.. Esperados</th>
+                                <th>Res.. Obtidos</th>
+                                <th>Ações</th>
 
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>Axel</td>
-                                    <td>Estes</td>
-                                    <td>33</td>
-                                    <td>Estes</td>
-                                    <td>
-                                        <a class=" waves-light btn tooltipped modal-trigger" 
-                                           data-position="bottom" href="#descicaoAtividade" 
-                                           data-tooltip="Visualizar Descrição" name="btn-visualizarDescicaoAtividade">
-                                            <i class="material-icons">forum</i>
-                                        </a>
-                                    </td>
-                                    <td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Axel</td>
+                                <td>Estes</td>
+                                <td>33</td>
+                                <td>Estes</td>
+                                <td>
+                                    <a class=" waves-light btn tooltipped modal-trigger" 
+                                       data-position="bottom" href="#descicaoAtividade" 
+                                       data-tooltip="Visualizar Descrição" name="btn-visualizarDescicaoAtividade">
+                                        <i class="material-icons">forum</i>
+                                    </a>
+                                </td>
+                                <td>
 
-                                        <a class=" waves-light btn tooltipped modal-trigger" 
-                                           data-position="bottom" href="#resultadosEsperadosAtividade" 
-                                           data-tooltip="Visualizar Resultados Esperados" name="btn-visualizarResultadosEsperadosAtividade">
-                                            <i class="material-icons">forum</i>
-                                        </a>
+                                    <a class=" waves-light btn tooltipped modal-trigger" 
+                                       data-position="bottom" href="#resultadosEsperadosAtividade" 
+                                       data-tooltip="Visualizar Resultados Esperados" name="btn-visualizarResultadosEsperadosAtividade">
+                                        <i class="material-icons">forum</i>
+                                    </a>
 
-                                    </td>
-                                    <td>
+                                </td>
+                                <td>
 
-                                        <a class=" waves-light btn tooltipped modal-trigger" 
-                                           data-position="bottom" href="#resultadosObtidosAtividade" 
-                                           data-tooltip="Visualizar Resultados Obtidos" name="btn-visualizarResultadosObtidosAtividade">
-                                            <i class="material-icons">forum</i>
-                                        </a>
+                                    <a class=" waves-light btn tooltipped modal-trigger" 
+                                       data-position="bottom" href="#resultadosObtidosAtividade" 
+                                       data-tooltip="Visualizar Resultados Obtidos" name="btn-visualizarResultadosObtidosAtividade">
+                                        <i class="material-icons">forum</i>
+                                    </a>
 
-                                    </td>
-                                    <td>
-                                        <a class="btn tooltipped" data-position="bottom" 
-                                           data-tooltip="Selecione para Editar Atividade" name="btn-editarAtividade">
-                                            <i class="material-icons">edit</i>
-                                        </a>
+                                </td>
+                                <td>
+                                    <a class="btn tooltipped" data-position="bottom" 
+                                       data-tooltip="Selecione para Editar Atividade" name="btn-editarAtividade">
+                                        <i class="material-icons">edit</i>
+                                    </a>
 
-                                        <a class="btn tooltipped" data-position="bottom" 
-                                           data-tooltip="Selecione para Apagar Atividade" name="btn-deletarAtividade">
-                                            <i class="material-icons">delete</i>                                                                            
-                                        </a>
-                                    </td>
-                                </tr>                                 
-                            </tbody>
-                        </table>
-                        <div class="col-md-12 center text-center">
-                            <span class="left" id="total_reg"></span>
-                            <ul class="pagination pager" id="paginaFicha"></ul>
-                        </div>
+                                    <a class="btn tooltipped" data-position="bottom" 
+                                       data-tooltip="Selecione para Apagar Atividade" name="btn-deletarAtividade">
+                                        <i class="material-icons">delete</i>                                                                            
+                                    </a>
+                                </td>
+                            </tr>                                 
+                        </tbody>
+                    </table>
+                    <div class="col-md-12 center text-center">
+                        <span class="left" id="total_reg"></span>
+                        <ul class="pagination pager" id="paginaFicha"></ul>
                     </div>
                 </div>
             </div>
         </div>
-        <script type="text/javascript" src="js/materialize.js"></script>
-        <script>
-            $(document).ready(function () {
-                $('.modal').modal();
-                $('.tooltipped').tooltip();
-                $('#tabelaAtividade').pageMe({
-                    pagerSelector: '#paginaAtividade',
-                    activeColor: 'green darken-4',
-                    prevText: 'Anterior',
-                    nextText: 'Siguiente',
-                    showPrevNext: true,
-                    hidePageNumbers: false,
-                    perPage: 5
-                });
-                $('#tabelaFicha').pageMe({
-                    pagerSelector: '#paginaFicha',
-                    activeColor: 'green darken-4',
-                    prevText: 'Anterior',
-                    nextText: 'Siguiente',
-                    showPrevNext: true,
-                    hidePageNumbers: false,
-                    perPage: 5
-                });
-                $('select').formSelect();
-                $('.datepicker').datepicker();
-                $('.collapsible').collapsible();
-                $('select').formSelect();
-                $('.sidenav').sidenav();
-                $(".dropdown-trigger").dropdown({
-                    inDuration: 30
-                });
-
-
+    </div>
+    <script type="text/javascript" src="js/materialize.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('.modal').modal();
+            $('.tooltipped').tooltip();
+            $('#tabelaAtividade').pageMe({
+                pagerSelector: '#paginaAtividade',
+                activeColor: 'green darken-4',
+                prevText: 'Anterior',
+                nextText: 'Siguiente',
+                showPrevNext: true,
+                hidePageNumbers: false,
+                perPage: 5
             });
-        </script>
-    </body>
+            $('#tabelaFicha').pageMe({
+                pagerSelector: '#paginaFicha',
+                activeColor: 'green darken-4',
+                prevText: 'Anterior',
+                nextText: 'Siguiente',
+                showPrevNext: true,
+                hidePageNumbers: false,
+                perPage: 5
+            });
+            $('select').formSelect();
+            $('.datepicker').datepicker();
+            $('.collapsible').collapsible();
+            $('select').formSelect();
+            $('.sidenav').sidenav();
+            $(".dropdown-trigger").dropdown({
+                inDuration: 30
+            });
+
+
+        });
+    </script>
+</body>
 </html>
